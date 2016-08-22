9db629eb604c25b262e6b9099aa5aded8b3eeafd
Christopher Quackenbush <christopher@quackenbush.me>
2016-08-22 10:17:41 -0700

    Bumped version to v0.1.0 to signify that the beta version is now on the master branch

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -61,7 +61,7 @@
 
     <footer class="main-footer">
         <div class="pull-right hidden-xs">
-            <b>Version</b> 0.0.1
+            <b>Version</b> 0.1.0
         </div>
         <strong>Copyright &copy; 2016 <a href="#">Radium</a>.</strong> All rights reserved.
     </footer>
